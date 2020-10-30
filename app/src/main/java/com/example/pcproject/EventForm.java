99f60e8dbf6d8a174a5b77f379d88c364afc5588
pmartinez14323 <phmart@iu.edu>
2020-10-29 20:07:01 -0400

    Updated event activity form

--- app/src/main/java/com/example/pcproject/EventForm.java
+++ app/src/main/java/com/example/pcproject/EventForm.java
@@ -3,12 +3,51 @@ package com.example.pcproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventForm extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_form);
+
+        List<String> events = new ArrayList<String>();
+        events.add("Fight");
+        events.add("Date");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, events);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerEvent);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Object item = adapterView.getItemAtPosition(i);
+                if(item != null)
+                {
+                    Toast.makeText(EventForm.this, item.toString(), Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(EventForm.this, "Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
