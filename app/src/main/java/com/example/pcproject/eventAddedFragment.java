e5d61eb3d4478a193464883da436272cbe59483a
pmartinez14323 <phmart@iu.edu>
2020-11-11 00:18:02 -0500

    Minor layout fixes to landing page. Added a timer to eventAddedFragment that will automatically take user to landing page after 7 seconds.

--- app/src/main/java/com/example/pcproject/eventAddedFragment.java
+++ app/src/main/java/com/example/pcproject/eventAddedFragment.java
@@ -1,5 +1,6 @@
 package com.example.pcproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,9 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class eventAddedFragment extends Fragment {
-    private Button eventDoneB;
+    Timer timer;
 
     public eventAddedFragment() {
         // Required empty public constructor
@@ -22,7 +26,16 @@ public class eventAddedFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_event_added, container, false);
-        eventDoneB = v.findViewById(R.id.eventDoneB);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getActivity(), LandingPage.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        }, 7000);
 
         return v;
     }
